15b0241dbd817dd6371cf3fa52c03eec8650e145
aohara-cc <aohara@camcloud.com>
2022-04-27 13:54:32 -0400

    :arrow_up: :bug: upgrade dependencies and remove usage of inline function that could break between versions

--- src/main/kotlin/io/andrewohara/utils/config/configLoaderHttp4k.kt
+++ src/main/kotlin/io/andrewohara/utils/config/configLoaderHttp4k.kt
@@ -5,7 +5,7 @@ import org.http4k.core.*
 import org.http4k.filter.ClientFilters
 import java.io.IOException
 
-fun ConfigLoader.Companion.http4k(backend: HttpHandler = JavaHttpClient()) = ConfigLoader { name ->
+fun ConfigLoader.Companion.http4k(backend: HttpHandler) = ConfigLoader { name ->
     val response = backend(Request(Method.GET, name))
     when(response.status) {
         Status.OK -> response.body.stream.readBytes()
@@ -16,10 +16,10 @@ fun ConfigLoader.Companion.http4k(backend: HttpHandler = JavaHttpClient()) = Con
 
 fun ConfigLoader.Companion.http4k(
     baseUri: String,
-    backend: HttpHandler = JavaHttpClient(),
+    backend: HttpHandler
 ) = http4k(Uri.of(baseUri), backend)
 
 fun ConfigLoader.Companion.http4k(
     baseUri: Uri,
-    backend: HttpHandler = JavaHttpClient(),
+    backend: HttpHandler
 ) = http4k(ClientFilters.SetBaseUriFrom(baseUri).then(backend))
