aa8e5ed58d780089365aeee8b37381bb096ae3fb
aohara <aohara@camcloud.com>
2022-07-15 13:10:24 -0400

    :sparkles: add option to splitIo FeatureFlags to block until ready

--- src/main/kotlin/io/andrewohara/utils/features/splitFeatureFlags.kt
+++ src/main/kotlin/io/andrewohara/utils/features/splitFeatureFlags.kt
@@ -4,9 +4,10 @@ import io.split.client.SplitClient
 import io.split.client.SplitClientConfig
 import io.split.client.SplitFactoryBuilder
 
-fun FeatureFlags.Companion.splitIo(apiKey: String) = SplitClientConfig
+fun FeatureFlags.Companion.splitIo(apiKey: String, blockUntilReady: Boolean = false) = SplitClientConfig
     .builder().build()
     .let { config -> SplitFactoryBuilder.build(apiKey, config).client() }
+    .also { if (blockUntilReady) it.blockUntilReady() }
     .let { FeatureFlags.splitIo(it) }
 
 fun FeatureFlags.Companion.splitIo(client: SplitClient) = FeatureFlags { feature ->
