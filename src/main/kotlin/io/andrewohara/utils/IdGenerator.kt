c7212c330dd6b36b4589af1c744ecc0a868bbacb
Andrew O'Hara <ohara.andrew314@gmail.com>
2024-03-24 22:47:28 -0400

    remove IdGenerator.nextNumeric

--- src/main/kotlin/io/andrewohara/utils/IdGenerator.kt
+++ src/main/kotlin/io/andrewohara/utils/IdGenerator.kt
@@ -7,7 +7,6 @@ open class IdGenerator(private val random: Random = Random()) {
     companion object: IdGenerator()
 
     private val base36 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()
-    private val numeric = "0123456789".toCharArray()
     private val hex = "0123456789ABCDEF".toCharArray()
 
     private fun next(length: Int, chars: CharArray): String {
@@ -19,8 +18,6 @@ open class IdGenerator(private val random: Random = Random()) {
     }
 
     fun nextBase36(length: Int) = next(length, base36)
-
-    fun nextNumeric(length: Int) = next(length, numeric).toBigInteger()
 
     fun nextHex(length: Int) = next(length, hex)
 }
--- src/test/kotlin/io/andrewohara/utils/IdGeneratorTest.kt
+++ src/test/kotlin/io/andrewohara/utils/IdGeneratorTest.kt
@@ -20,14 +20,8 @@ class IdGeneratorTest {
     }
 
     @Test
-    fun `next numeric`() {
-        testObj.nextNumeric(8) shouldBe 10546784.toBigInteger()
-    }
-
-    @Test
     fun `companion object`() {
         IdGenerator.nextHex(4).shouldHaveLength(4)
-        IdGenerator.nextNumeric(4).toString().shouldHaveLength(4)
         IdGenerator.nextBase36(4).shouldHaveLength(4)
     }
 }
