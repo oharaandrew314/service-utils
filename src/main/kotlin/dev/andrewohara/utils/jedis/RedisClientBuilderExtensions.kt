a046a3f059720fea0663d44d5800913afb98b3cc
Andrew O'Hara <aohara@camcloud.com>
2026-08-12 11:34:34 -0400

    redis client builder error handling

--- src/main/kotlin/dev/andrewohara/utils/jedis/RedisClientBuilderExtensions.kt
+++ src/main/kotlin/dev/andrewohara/utils/jedis/RedisClientBuilderExtensions.kt
@@ -5,15 +5,15 @@ import redis.clients.jedis.DefaultJedisClientConfig
 import redis.clients.jedis.RedisClient
 import redis.clients.jedis.SslOptions
 import redis.clients.jedis.SslVerifyMode
-import redis.clients.jedis.util.JedisURIHelper
-import java.net.URI
+
+private val localhosts get()= setOf("localhost", "127.0.0.1")
 
 fun RedisClient.Builder.uri(uri: Uri) = this
-    .hostAndPort(JedisURIHelper.getHostAndPort(URI.create(uri.toString())))
+    .hostAndPort(uri.host, uri.port ?: 6379)
     .clientConfig(DefaultJedisClientConfig.builder().apply {
         if (uri.scheme == "rediss") {
             sslOptions(SslOptions.builder()
-                .sslVerifyMode(if (uri.host == "localhost") SslVerifyMode.CA else SslVerifyMode.FULL)
+                .sslVerifyMode(if (uri.host in localhosts) SslVerifyMode.CA else SslVerifyMode.FULL)
                 .build())
         }
     }.build())
